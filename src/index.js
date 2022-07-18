99f8469cbac80f987641043931d365a9bf361ccd
AgathePons <ponsagathe37@gmail.com>
2022-07-18 12:33:19 +0200

    check eslint is working

--- src/index.js
+++ src/index.js
@@ -6,19 +6,16 @@ import { render } from 'react-dom';
 import { DireBonjour, DireAuRevoir } from './components/DireBonjour';
 import './style.css';
 
-const App = (   ) => {
-  return (
-    <div>
+const App = () => (
+  <div>
 
-
-      <h1>Hello World!</h1>   
-      <p style={{ color: 'pink', backgroundColor: 'purple' }}>Trop stylé React</p>
-      <DireBonjour prenom="Julien" nom="Delatour" age={ 42 } />
-      <DireBonjour prenom="Killian" nom="Allouard" age={ 12 } />
-      <DireBonjour prenom="Timéo" nom="Bernard" age={ 10 } />
-      <DireAuRevoir prenom="Camille" nom="Roublond" />
-    </div>
-  )
-};
+    <h1>Hello World!</h1>
+    <p style={{ color: 'pink', backgroundColor: 'purple' }}>Trop stylé React</p>
+    <DireBonjour prenom="Julien" nom="Delatour" age={42} />
+    <DireBonjour prenom="Killian" nom="Allouard" age={12} />
+    <DireBonjour prenom="Timéo" nom="Bernard" age={10} />
+    <DireAuRevoir prenom="Camille" nom="Roublond" />
+  </div>
+);
 
 render(<App />, document.getElementById('root'));
